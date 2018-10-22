f27d45448195bb06d9a37e31e4b6e20952cdc190
peter lai <e2201lai@gmail.com>
2018-10-22 08:53:34 +0800

    edit navigation

--- gzhipin-rn/navigation/MainTabNavigator.js
+++ gzhipin-rn/navigation/MainTabNavigator.js
@@ -3,12 +3,20 @@ import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
+// import HomeScreen from '../screens/HomeScreen';
+// import LinksScreen from '../screens/LinksScreen';
+// import SettingsScreen from '../screens/SettingsScreen';
+
+import LaobanScreen from '../screens/laoban/laoban';
+import LaobanScreenInfo from '../screens/laoban-info/laoban-info';
+
+import DashenScreen from '../screens/dashen/dashen';
+import DashenScreenInfo from '../screens/dashen-info/dashen-info';
+
 import SettingsScreen from '../screens/SettingsScreen';
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
+const LaobanStack = createStackNavigator({
+  Laoban: LaobanScreen,
 });
 
 HomeStack.navigationOptions = {
@@ -52,6 +60,52 @@ SettingsStack.navigationOptions = {
     />
   ),
 };
+
+// const HomeStack = createStackNavigator({
+//   Home: HomeScreen,
+// });
+
+// HomeStack.navigationOptions = {
+//   tabBarLabel: 'Home',
+//   tabBarIcon: ({ focused }) => (
+//     <TabBarIcon
+//       focused={focused}
+//       name={
+//         Platform.OS === 'ios'
+//           ? `ios-information-circle${focused ? '' : '-outline'}`
+//           : 'md-information-circle'
+//       }
+//     />
+//   ),
+// };
+
+// const LinksStack = createStackNavigator({
+//   Links: LinksScreen,
+// });
+
+// LinksStack.navigationOptions = {
+//   tabBarLabel: 'Links',
+//   tabBarIcon: ({ focused }) => (
+//     <TabBarIcon
+//       focused={focused}
+//       name={Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link'}
+//     />
+//   ),
+// };
+
+// const SettingsStack = createStackNavigator({
+//   Settings: SettingsScreen,
+// });
+
+// SettingsStack.navigationOptions = {
+//   tabBarLabel: 'Settings',
+//   tabBarIcon: ({ focused }) => (
+//     <TabBarIcon
+//       focused={focused}
+//       name={Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options'}
+//     />
+//   ),
+// };
 
 export default createBottomTabNavigator({
   HomeStack,
